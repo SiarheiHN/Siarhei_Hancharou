627e0217895f8a9a2c4dd8cac9f5fddad8da836e
SiarheiHN <hancharousn@gmail.com>
2022-10-05 21:54:59 +0300

    change_SECRET_KEY

--- MY_DJANGO_PROJECT/src/proj/settings.py
+++ MY_DJANGO_PROJECT/src/proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from . import local_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqz2du5%p_w4vcn1^^u76k1mr9llm=2(xktln01pgpib^u#6-p'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
